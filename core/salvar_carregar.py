e7b6c9dfa7ba8c52dc4c342d1302d6288e946c13
Erik <erikSlava@proton.me>
2026-08-31 03:25:46 -0300

    Fix typo in function name from 'slavar_dados' to 'salvar_dados'

--- core/salvar_carregar.py
+++ core/salvar_carregar.py
@@ -5,7 +5,7 @@ def carregar_arq(caminho):
     dados = json.load(arq)
   return dados
 
-def slavar_dados(caminho, dados):
+def salvar_dados(caminho, dados):
   with open(f"{caminho}", "w") as arq:
     json.dump(dados, arq, indent=4)
   return True
