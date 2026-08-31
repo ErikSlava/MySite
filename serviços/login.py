38d70a725f16546a4f9b1c72dd9bf6a68dd49485
Erik <erikSlava@proton.me>
2026-08-31 03:05:44 -0300

    Implement login function for user authentication

---
+++ serviços/login.py
@@ -0,0 +1,15 @@
+from core.salvar_carregar import carregar_arq
+
+arquivo = carregar_arq("../data/usuarios.json")
+def login(cpf, nome):
+  try:
+    if cpf in arquivo:
+      usuario = arquivo["cpf"]
+      if usuario["senha"] == senha:
+        return True, "login bem sucedido"
+      else:
+        return False, "usuario ou senha inválida"
+    else:
+      return False, "usuario ou senha inválida"
+  except Exception as e:
+    return False, "Error na aplicaçao"
