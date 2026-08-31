94992cdee12343471271c297871160938c3daeed
Erik <erikSlava@proton.me>
2026-08-31 02:51:31 -0300

    Update cadastrar.py

--- serviços/cadastrar.py
+++ serviços/cadastrar.py
@@ -1,19 +1,23 @@
-usuarios = []
-def validar(cpf, nome, email, cidade):
+from core.salvar_carregar import carregar_arq, salvar_dados
+
+arquivo = carregar_arq("../data/usuarios.json")
+def validar(cpf, nome, email, cidade, nascimento, senha):
     try:
-       for usuario in usuarios:
+       for usuario in arquivos:
           if usuario["cpf"] == cpf:
              return False, "cpf ja cadastrado"
           if usuario["email"]  == email:
              return False, "email ja cadastrado"
 
-       novo_usuario = {
+       novo_usuario["cpf] = {
            "cpf": cpf,
            "nome": nome,
            "email": email,
-           "cidade": cidade
-       }
-       usuarios.append(novo_usuario)
+           "cidade": cidade,
+           "nascimento": nascimento,
+           "senha": senha
+        }
+       salvar_dados(novo_usuario)
        return True, "usuario cadastrado com sucesso"
 
     except Exception as e:
