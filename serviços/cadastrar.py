1e1813ed17a48dcb45491679f6dcf1b24f367a4d
Erik <erikSlava@proton.me>
2026-09-04 20:23:57 -0300

    Refactor user registration to use dictionary update

--- serviços/cadastrar.py
+++ serviços/cadastrar.py
@@ -1,27 +1,38 @@
 from core.salvar_carregar import carregar_arq, salvar_dados
 
-arquivo = carregar_arq("data/usuarios.json")
+arquivos = carregar_arq("data/usuarios.json")
 def cadastrar(cpf, nome, email, cidade, nascimento, senha):
     try:
-       for usuario in arquivo:
+       for usuario in arquivos:
           if usuario["cpf"] == cpf:
              return False, "cpf ja cadastrado"
           if usuario["email"]  == email:
              return False, "email ja cadastrado"
 
-       novo_usuario["cpf"] = {
-           "cpf": cpf,
-           "nome": nome,
-           "email": email,
-           "cidade": cidade,
-           "tipo": "usuario",
-           "saldo": 1000,
-           "nascimento": nascimento,
-           "senha": senha,
-           "ativo": True
+       novo_usuario = {
+            cpf: {
+                "cpf": cpf,
+                "nome": nome,
+                "email": email,
+                "cidade": cidade,
+                "tipo": "usuario",
+                "saldo": 1000,
+                "nascimento": nascimento,
+                "senha": senha,
+                "ativo": True
+            }
         }
-       salvar_dados(novo_usuario)
+       arquivos.update(novo_usuario)
+       salvar_dados(arquivos)
        return True, "usuario cadastrado com sucesso"
 
     except Exception as e:
       return False, f"erro na aplicaçao {e}"
+
+
+
+
+
+
+
+
